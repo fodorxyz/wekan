f173a6029cc333d01bcd70682988363a7419d7fa
Ashley Hindle <ashley@smellynose.com>
2016-07-08 23:07:57 +0100

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -17,7 +17,7 @@ wekan:
     - wekandb
   environment:
     - MONGO_URL=mongodb://wekandb/wekan
-    - ROOT_URL=http://mytodo.org
+    - ROOT_URL=http://${DOMAIN}
     - MAIL_URL=smtp://user:pass@mailserver.example.com:25/
     - MAIL_FROM=wekan-admin@example.com
   ports:
@@ -31,28 +31,28 @@ EOF
 
 cat << EOF > /etc/apache2/sites-enabled/$DOMAIN.conf
 <VirtualHost *:80>
-        ServerName mytodo.org
-        ServerAdmin webmaster@mytodo.org
+        ServerName ${DOMAIN}
+        ServerAdmin webmaster@${DOMAIN}
 
-        DocumentRoot /var/www-vhosts/mytodo.org
+        DocumentRoot /var/www-vhosts/${DOMAIN}
         <Directory />
                 Options FollowSymLinks
                 AllowOverride AuthConfig FileInfo Indexes Options=MultiViews
         </Directory>
 
-        <Directory /var/www-vhosts/mytodo.org>
+        <Directory /var/www-vhosts/${DOMAIN}>
                 Options -Indexes +FollowSymLinks +MultiViews
                 AllowOverride AuthConfig FileInfo Indexes Options=MultiViews
                 Require all granted
         </Directory>
 
-        ErrorLog /var/log/apache2/mytodo.org-error.log
+        ErrorLog /var/log/apache2/${DOMAIN}-error.log
 
         # Possible values include: debug, info, notice, warn, error, crit,
         # alert, emerg.
         LogLevel warn
 
-        CustomLog /var/log/apache2/mytodo.org-access.log combined
+        CustomLog /var/log/apache2/${DOMAIN}-access.log combined
         ServerSignature Off
         
         ProxyPassMatch   "^/(sockjs\/.*\/websocket)$" "ws://127.0.0.1:8081/$1"
