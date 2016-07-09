7dec670b43b8b6ce61e67c94b935e41bbfe3eedc
Ashley Hindle <ashley@smellynose.com>
2016-07-09 14:26:54 +0100

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -1,3 +1,4 @@
+set -x
 curl -L https://github.com/docker/compose/releases/download/1.8.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
